76b6c216d93e774a27cd9465b7a30b8062276c0b
jarstywn <stywn.jar@gmail.com>
2025-06-11 15:57:07 +0700

    update index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,25 @@
+import { Link } from "react-router";
+
 export function meta() {
   return [{ title: "Hello world!" }];
 }
 
-
 export default function Page() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-2">
-        <h1>Hello world!</h1>
-        <button className="btn btn-outline btn-sm">hi!</button>
+      <div className="flex flex-col gap-3">
+        <div>
+          <h1 className="font-bold text-4xl">Hello world!</h1>
+          <p>just a simple crud based app with notes implementation</p>
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          <Link to={"/notes"} className="btn btn-ghost btn-sm">
+            Read your notes
+          </Link>
+          <Link to={"/create"} className="btn btn-outline btn-sm">
+            Create note
+          </Link>
+        </div>
       </div>
     </main>
   );
